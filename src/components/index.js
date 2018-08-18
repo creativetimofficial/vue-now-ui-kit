325125cd775f2fa801986a26b75dc774d792da1a
cristij <joracristi@gmail.com>
2018-08-18 18:45:01 +0300

    Lint files

--- src/components/index.js
+++ src/components/index.js
@@ -1,27 +1,25 @@
-import Alert from './Alert'
-import Checkbox from './Inputs/Checkbox.vue'
-import Radio from './Inputs/Radio.vue'
-import FormGroupInput from './Inputs/formGroupInput.vue'
-import Switch from './Switch.vue'
-import Badge from './Badge'
-import Progress from './Progress.vue'
-import Button from './Button.vue'
+import Alert from './Alert';
+import Checkbox from './Inputs/Checkbox.vue';
+import Radio from './Inputs/Radio.vue';
+import FormGroupInput from './Inputs/formGroupInput.vue';
+import Switch from './Switch.vue';
+import Badge from './Badge';
+import Progress from './Progress.vue';
+import Button from './Button.vue';
 
-import DropDown from './Dropdown.vue'
+import DropDown from './Dropdown.vue';
 
-import Card from './Cards/Card.vue'
+import Card from './Cards/Card.vue';
 import Navbar from './Navbar/Navbar';
 import NavLink from './Navbar/NavLink';
 import NavbarToggleButton from './Navbar/NavbarToggleButton';
-import TabPane from './Tabs/Tab.vue'
-import Tabs from './Tabs/Tabs.vue'
-import Modal from './Modal.vue'
-import Parallax from './Parallax.vue'
-import Slider from './Slider.vue'
+import TabPane from './Tabs/Tab.vue';
+import Tabs from './Tabs/Tabs.vue';
+import Modal from './Modal.vue';
+import Parallax from './Parallax.vue';
+import Slider from './Slider.vue';
 
-import Pagination from './Pagination.vue'
-
-import InfoSection from './InfoSection'
+import Pagination from './Pagination.vue';
 
 export {
   Alert,
@@ -42,6 +40,5 @@ export {
   Modal,
   Parallax,
   Slider,
-  Button,
-  InfoSection
-}
+  Button
+};
