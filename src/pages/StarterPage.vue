43e0b647b495ba9983e9e03b41f9eae0b6ea7286
cristij <joracristi@gmail.com>
2018-08-19 16:34:54 +0300

    Add starter layout

---
+++ src/pages/StarterPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="starter-page">
+        <div class="section text-center">
+            <h3>Create your next awesome website</h3>
+        </div>
+    </div>
+</template>
+<script>
+  export default {
+    name: 'starter'
+  };
+</script>
+<style>
+    .starter-page {
+        min-height: calc(100vh - 95px);
+    }
+</style>
---
+++ src/starterRouter.js
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import Starter from './pages/StarterPage.vue';
+import StarterNavbar from './layout/StarterNavbar.vue';
+import StarterFooter from './layout/StarterFooter.vue';
+
+Vue.use(Router);
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'index',
+      components: { default: Starter, header: StarterNavbar, footer: StarterFooter },
+      props: {
+        header: { colorOnScroll: 400 },
+        footer: { backgroundColor: 'black' }
+      }
+    }
+  ],
+  scrollBehavior: to => {
+    if (to.hash) {
+      return { selector: to.hash };
+    } else {
+      return { x: 0, y: 0 };
+    }
+  }
+});
