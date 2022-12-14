694401ad4c1c80f69cf91525a7457c9a56b4fe06
Maisam Afshar <maisam.afshar@gmail.com>
2022-12-14 13:13:48 +0430

    update date & version

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 /*!
 
  =========================================================
- * Vue Now UI Kit - v1.1.0
+ * Vue Now UI Kit - v1.2.0
  =========================================================
 
  * Product Page: https://www.creative-tim.com/product/now-ui-kit
- * Copyright 2019 Creative Tim (http://www.creative-tim.com)
+ * Copyright 2022 Creative Tim (http://www.creative-tim.com)
 
  * Designed by www.invisionapp.com Coded by www.creative-tim.com
 
@@ -14,11 +14,11 @@
  * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 
  */
-import Vue from 'vue';
-import App from './App.vue';
+import Vue from "vue";
+import App from "./App.vue";
 // You can change this import to `import router from './starterRouter'` to quickly start development from a blank layout.
-import router from './router';
-import NowUiKit from './plugins/now-ui-kit';
+import router from "./router";
+import NowUiKit from "./plugins/now-ui-kit";
 
 Vue.config.productionTip = false;
 
@@ -26,5 +26,5 @@ Vue.use(NowUiKit);
 
 new Vue({
   router,
-  render: h => h(App)
-}).$mount('#app');
+  render: (h) => h(App),
+}).$mount("#app");
